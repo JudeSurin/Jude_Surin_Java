7aa15e247ec24d1b317752074b1a86f083de651b
JudeSurin <74893645+JudeSurin@users.noreply.github.com>
2023-08-12 17:45:18 -0400

    Update CustomerTest.java

--- M1-Challenge-Surin-Jude/src/main/java/com/company/CustomerTest.java
+++ M1-Challenge-Surin-Jude/src/main/java/com/company/CustomerTest.java
@@ -1,35 +1,52 @@
 package com.company;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-public class CustomerTest {
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CustomerTest {
+
+    private static final List<String[]> customerData = Arrays.asList(
+            new String[] {"1","Wayne Enterprises","10000","12-01-2031"},
+            new String[] {"2","Daily Planet","-7500","01-10-2022"},
+            new String[] {"1","Wayne Enterprises","18000","12-22-2021"},
+            new String[] {"3","Ace Chemical","-48000","01-10-2022"},
+            new String[] {"3","Ace Chemical","-95000","12-15-2021"},
+            new String[] {"1","Wayne Enterprises","175000","01-01-2022"},
+            new String[] {"1","Wayne Enterprises","-35000","12-09-2021"},
+            new String[] {"1","Wayne Enterprises","-64000","01-17-2022"},
+            new String[] {"3","Ace Chemical","70000","12-29-2022"},
+            new String[] {"2","Daily Planet","56000","12-13-2022"},
+            new String[] {"2","Daily Planet","-33000","01-07-2022"},
+            new String[] {"1","Wayne Enterprises","10000","12-01-2021"},
+            new String[] {"2","Daily Planet","33000","01-17-2022"},
+            new String[] {"3","Ace Chemical","140000","01-25-2022"},
+            new String[] {"2","Daily Planet","5000","12-12-2022"},
+            new String[] {"3","Ace Chemical","-82000","01-03-2022"},
+            new String[] {"1","Wayne Enterprises","10000","12-01-2021"}
+    );
+
+    AccountManager manager = new AccountManager(customerData);
+
 
     @Test
-    public void testBalance() {
-        Customer customer = new Customer();
+    void getBalance() {
+        manager.addCustomerInformation();
+        assertEquals(124000, manager.viewCustomers().get(0).getBalance());
+        assertEquals(53500, manager.viewCustomers().get(1).getBalance());
+        assertEquals(-15000, manager.viewCustomers().get(2).getBalance());
 
-        //Create some AccountRecords and add them to the customer
-        Object AccountRecord1 = newAccountRecord();
-        AccountRecord record1 = null;
-        record1.setCharge(1000);
-
-        AccountRecord record2 = new AccountRecord();
-        record2.setCharge(-500);
-
-        customer.getCharges().add(record1);
-        customer.getCharges().add(record2);
-
-        //Calculate the expected balance
-        int expectedBalance = 1000 - 500;
-
-        // Call the getBalance() method and assert the result
-        asserEquals(expectedBalance, customer.getBalance());
     }
 
-    private Object newAccountRecord() {
-        return null;
-    }
-
-    private void asserEquals(int expectedBalance, int balance) {
+    @Test
+    void testToString() {
+        manager.addCustomerInformation();
+        assertEquals("id: 1 name: Wayne Enterprises balance: 124000", manager.viewCustomers().get(0).toString());
+        assertEquals("id: 2 name: Daily Planet balance: 53500", manager.viewCustomers().get(1).toString());
+        assertEquals("id: 3 name: Ace Chemical balance: -15000", manager.viewCustomers().get(2).toString());
     }
 }
