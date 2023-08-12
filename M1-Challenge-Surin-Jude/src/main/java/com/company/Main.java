e442738b11a4b1cab801f9ef2f636a99f5e5fb79
JudeSurin <74893645+JudeSurin@users.noreply.github.com>
2023-08-12 17:42:49 -0400

    Update Main.java

--- M1-Challenge-Surin-Jude/src/main/java/com/company/Main.java
+++ M1-Challenge-Surin-Jude/src/main/java/com/company/Main.java
@@ -1,71 +1,38 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-
-    // Hardcoded list of customer data
-    private static List<String[]> customerData = Arrays.asList(
-            new String[]{"1", "Wayne Enterprises", "10000", "12-01-2021"},
-            new String[]{"2", "Daily Planet", "-7500", "01-10-2022"}
-            new String []{"1", "Wayne Enterprises", "18000"}, 
-            new String[] {"3","Ace Chemical","-48000"},
-            new String[] {"3","Ace Chemical","-95000"},
-            new String[] {"1","Wayne Enterprises","175000"},
-            new String[] {"1","Wayne Enterprises","-35000"},
-            new String[] {"1","Wayne Enterprises","-64000"},
-            new String[] {"3","Ace Chemical","70000"},
-            new String[] {"2","Daily Planet","56000"},
-            new String[] {"2","Daily Planet","-33000"},
-            new String[] {"1","Wayne Enterprises","10000"},
-            new String[] {"2","Daily Planet","33000"},
-            new String[] {"3","Ace Chemical","140000"},
-            new String[] {"2","Daily Planet","5000"},
-            new String[] {"3","Ace Chemical","-82000"},
-            new String[] {"1","Wayne Enterprises","10000"}
+    private static final List<String[]> customerData = Arrays.asList(
+            new String[] {"1","Wayne Enterprises","10000","12-01-2031"},
+            new String[] {"2","Daily Planet","-7500","01-10-2022"},
+            new String[] {"1","Wayne Enterprises","18000","12-22-2021"},
+            new String[] {"3","Ace Chemical","-48000","01-10-2022"},
+            new String[] {"3","Ace Chemical","-95000","12-15-2021"},
+            new String[] {"1","Wayne Enterprises","175000","01-01-2022"},
+            new String[] {"1","Wayne Enterprises","-35000","12-09-2021"},
+            new String[] {"1","Wayne Enterprises","-64000","01-17-2022"},
+            new String[] {"3","Ace Chemical","70000","12-29-2022"},
+            new String[] {"2","Daily Planet","56000","12-13-2022"},
+            new String[] {"2","Daily Planet","-33000","01-07-2022"},
+            new String[] {"1","Wayne Enterprises","10000","12-01-2021"},
+            new String[] {"2","Daily Planet","33000","01-17-2022"},
+            new String[] {"3","Ace Chemical","140000","01-25-2022"},
+            new String[] {"2","Daily Planet","5000","12-12-2022"},
+            new String[] {"3","Ace Chemical","-82000","01-03-2022"},
+            new String[] {"1","Wayne Enterprises","10000","12-01-2021"}
     );
 
+
+
     public static void main(String[] args) {
-        List<Customer> customers = new ArrayList<>();
+        AccountManager manager = new AccountManager(customerData);
+        manager.addCustomerInformation();
 
-        // Iterate through customerData, convert it to Customer objects, and add them to the customers list
-        for (String[] data : customerData) {
-            int id = Integer.parseInt(data[0]);
-            String name = data[1];
-            int charge = Integer.parseInt(data[2]);
-            Customer customer = findOrCreateCustomer(customers, id, name);
-            customer.addCharge(charge);
-        }
+        System.out.println(manager.viewCustomers());
 
-        // Print positive and negative account balances
-        System.out.println("Positive accounts:");
-        for (Customer customer : customers) {
-            if (customer.getBalance() > 0) {
-                System.out.println(customer);
-            }
-        }
-
-        System.out.println("Negative accounts:");
-        for (Customer customer : customers) {
-            if (customer.getBalance() < 0) {
-                System.out.println(customer);
-            }
-        }
-    }
-
-    // Method to find an existing customer or create a new customer if not found
-    private static Customer findOrCreateCustomer(List<Customer> customers, int id, String name) {
-        for (Customer customer : customers) {
-            if (customer.getId() == id) {
-                return customer;
-            }
-        }
-        Customer newCustomer = new Customer();
-        newCustomer.setId(id);
-        newCustomer.setName(name);
-        customers.add(newCustomer);
-        return newCustomer;
+        System.out.println("Positive accounts: " + manager.positiveAccounts());
+        System.out.println("Negative accounts: " + manager.negativeAccounts());
     }
 }
