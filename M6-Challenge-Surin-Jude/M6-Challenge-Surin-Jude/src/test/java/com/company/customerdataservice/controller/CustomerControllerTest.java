d2ccca8f36cfde111a4aad2c21fe65eadba9c3a8
JudeSurin <74893645+JudeSurin@users.noreply.github.com>
2023-08-15 06:14:39 -0400

    Add files via upload

---
+++ M6-Challenge-Surin-Jude/M6-Challenge-Surin-Jude/src/test/java/com/company/customerdataservice/controller/CustomerControllerTest.java
@@ -0,0 +1,34 @@
+package com.company.customerdataservice.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class CustomerControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCustomer() {
+    }
+
+    @Test
+    void createCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+
+    @Test
+    void getCustomersByState() {
+    }
+}
